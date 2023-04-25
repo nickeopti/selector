182799c748edb1707844902205d42d5791185146
Nicklas Boserup <nickeopti@gmail.com>
2023-04-25 17:23:19 +0200

    Initial commit

---
+++ selector/__init__.py
@@ -0,0 +1,5 @@
+from selector.arguments import (
+    add_arguments,
+    add_options,
+    add_options_from_module,
+)  # noqa: F401
---
+++ selector/arguments.py
@@ -0,0 +1,105 @@
+import inspect
+import typing
+from argparse import ArgumentParser, _StoreAction
+from functools import partial
+from types import ModuleType
+from typing import Sequence, Type, TypeVar, Union
+
+T = TypeVar("T")
+
+
+def add_arguments(
+    argument_parser: ArgumentParser, name: str, class_ref: Type[T]
+) -> partial[T]:
+    # TODO: Arguments not specified should be excluded, not set to None
+
+    argument_group = argument_parser.add_argument_group(name)
+
+    previously_known_arguments = [
+        argument.option_strings[0][2:]
+        for argument in argument_parser._actions
+        if isinstance(argument, _StoreAction)
+    ]
+    arguments = _get_arguments(class_ref)
+    for argument in arguments:
+        if argument.name in previously_known_arguments:
+            continue
+
+        if is_optional(argument.annotation):
+            argument_group.add_argument(
+                f"--{argument.name}",
+                type=typing.get_args(argument.annotation)[0],
+            )
+        else:
+            argument_group.add_argument(
+                f"--{argument.name}",
+                type=argument.annotation,
+            )
+
+    temp_args, _ = argument_parser.parse_known_args()
+    argument_values = {
+        argument.name: vars(temp_args)[argument.name]
+        for argument in arguments
+        if vars(temp_args)[argument.name] is not None
+    }
+
+    return partial(class_ref, **argument_values)
+
+
+def add_options(
+    argument_parser: ArgumentParser, name: str, options: Sequence[Type[T]]
+) -> partial[T]:
+    argument_group = argument_parser.add_argument_group(name)
+
+    argument_group.add_argument(
+        f"--{name}",
+        type=str,
+        default=options[0].__name__ if options[0] is not None else None,
+    )
+    temp_args, _ = argument_parser.parse_known_args()
+
+    selected_class_name = vars(temp_args)[name]
+
+    selected_class = {c.__name__: c for c in options if c is not None}[
+        selected_class_name
+    ]
+
+    return add_arguments(argument_parser, selected_class_name, selected_class)
+
+
+def add_options_from_module(
+    argument_parser: ArgumentParser,
+    name: str,
+    module: ModuleType,
+    of_subclass: Type[T],
+) -> partial[T]:
+    def predicate(obj):
+        return inspect.isclass(obj) and issubclass(obj, of_subclass)
+
+    valid_classes = inspect.getmembers(module, predicate)
+    options = [valid_class for _, valid_class in valid_classes]
+
+    return add_options(argument_parser, name, options)
+
+
+def _get_arguments(
+    from_object: Type[object], excluded_parameters=("self", "cls", "device")
+):
+    signature = inspect.signature(from_object.__init__)
+    parameters = {
+        k: p
+        for k, p in signature.parameters.items()
+        if p.kind == p.POSITIONAL_OR_KEYWORD
+    }
+    return [
+        parameter
+        for parameter in parameters.values()
+        if parameter.name not in excluded_parameters
+    ]
+
+
+def is_optional(annotation):
+    return (
+        typing.get_origin(annotation) is Union
+        and None.__class__ in typing.get_args(annotation)
+    )
