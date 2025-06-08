9acd7cc2ce9cc3ba33566c909eac859de93ede71
Nicklas Boserup <nickeopti@gmail.com>
2025-06-08 19:17:29 +0200

    Allow specifying args for get_argument as well

--- selector/arguments.py
+++ selector/arguments.py
@@ -24,9 +24,16 @@ CONVERTER = {
 }
 
 
-def get_argument(argument_parser: ArgumentParser, name: str, type: Type[T], default: T | None = None) -> T:
+def get_argument(
+    argument_parser: ArgumentParser,
+    name: str,
+    type: Type[T],
+    default: T | None = None,
+    *,
+    args: Sequence[str] | None = None,
+) -> T:
     argument_parser.add_argument(f'--{name}', type=CONVERTER.get(type, type), default=default)
-    args, _ = argument_parser.parse_known_args()
+    args, _ = argument_parser.parse_known_args(args)
     return getattr(args, name)
 
 
