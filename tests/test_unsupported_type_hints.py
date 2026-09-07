7cb112495882775382a3cdc9a4f9558c0077ce97
Nicklas Boserup <nickeopti@gmail.com>
2026-09-07 17:03:03 +0200

    Make unsupported type hints tests check for messages

--- tests/test_unsupported_type_hints.py
+++ tests/test_unsupported_type_hints.py
@@ -9,7 +9,7 @@ def test_any_type_hint():
     def f(x: Any):
         return x
 
-    with pytest.warns(UserWarning):
+    with pytest.warns(UserWarning, match='is Any which is unsupported'):
         selector.add_arguments('f', f)
 
 
@@ -17,7 +17,7 @@ def test_missing_type_hint():
     def f(x):  # type: ignore
         return x  # type: ignore
     
-    with pytest.warns(UserWarning):
+    with pytest.warns(UserWarning, match='seems to be missing'):
         selector.add_arguments('f', f)  # type: ignore
 
 
@@ -25,13 +25,19 @@ def test_missing_item_type_hint():
     def f(x: list[Any]):  # type: ignore
         return x  # type: ignore
     
-    with pytest.warns(UserWarning):
+    with pytest.warns(UserWarning, match='missing type hint for collection items'):
         selector.add_arguments('f', f)  # type: ignore
+
+    def g(x: list):  # type: ignore
+        return x  # type: ignore
+
+    with pytest.warns(UserWarning, match='missing type hint for collection items'):
+        selector.add_arguments('g', g)  # type: ignore
 
 
 def test_multiple_types():
     def f(x: int | str):
         return x
 
-    with pytest.warns(UserWarning):
+    with pytest.warns(UserWarning, match='is multiple types which is unsupported'):
         selector.add_arguments('f', f)
